59a6b459bb08511c2b30eef575c9289c69ef28d0
Luigi <luigi.derussis@polito.it>
2014-05-07 15:03:27 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flix::Application.config.secret_token = '56cd2ab6c631149ed5654447ca07f9422db22f4120f8f2f96257284548ada59335390306b6a343c23a09f7ac4776d3dc9613c48acb55575028af5aa26d8c8121'
