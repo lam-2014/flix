57eb04d2a8bdba944610634a11be9c84891a9b76
Luigi <luigi.derussis@polito.it>
2014-05-21 15:44:54 +0200

    add the default routes for the Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Flix::Application.routes.draw do
 
   match '/signup', to: 'users#new'
 
+  # default routes for the Users controller
+  resources :users
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
