b5c68d19cee11be8e98e48ff867037350c737d25
Luigi <luigi.derussis@polito.it>
2014-05-26 10:37:46 +0200

    add the required validation to the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title, :user_id
+  # only content and title must be accessible, in order to avoid manual (and wrong) associations between posts and users
+  attr_accessible :content, :title
+
+  # user_id must be present while creating a new post...
+  validates :user_id, presence: true
+
+  # title must be present and longer than 5 chars
+  validates :content, presence: true, length: {minimum: 5}
+
+  # content must be present and not longer than 500 chars
+  validates :content, presence: true, length: {maximum: 500}
+
 end
